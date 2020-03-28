48528719f76059915c747c036e155e274594d6ab
Aviv Landesman <aviv13523@gmail.com>
2020-03-28 15:07:58 +0300

    Added chain operations

--- Form1.cs
+++ Form1.cs
@@ -30,7 +30,7 @@ namespace SimpleCalculator
             }
             else
             {
-
+                result.Text = result.Text + numBtn.Text;
             }
             
         }
@@ -53,11 +53,24 @@ namespace SimpleCalculator
 
         private void operator_click(object sender, EventArgs e)
         {
-            operation_pressed = true;
             Button opBtn = (Button)sender;
-            operation = opBtn.Text;
-            value = Double.Parse(result.Text);
-            equation.Text = value + operation;
+
+
+            if (value != 0)
+            {
+                button15.PerformClick();//click result button
+                operation_pressed = true;
+                operation = opBtn.Text;
+                equation.Text = value +  " " + operation;
+
+            }
+            else
+            {
+                operation_pressed = true;
+                operation = opBtn.Text;
+                value = Double.Parse(result.Text);
+                equation.Text = value + " " + operation;
+            }
         }
 
         private void res_click(object sender, EventArgs e)
@@ -85,6 +98,8 @@ namespace SimpleCalculator
                 default:
                     break;
             }
+            value = Double.Parse(result.Text);
+            operation = "";
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
