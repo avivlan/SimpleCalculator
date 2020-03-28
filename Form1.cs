a1a10d5214c8278aee7d4157aaa86bc03a71719c
Aviv Landesman <aviv13523@gmail.com>
2020-03-28 04:55:01 +0300

    Code clean up

--- Form1.cs
+++ Form1.cs
@@ -1,33 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
-        Double value = 0;
-        String operation = "";
-        bool operation_pressed = false;
+        private String operation = "";
+        private bool operation_pressed = false;
+        private Double value = 0;
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void button_Click(object sender, EventArgs e)
@@ -37,14 +20,24 @@ namespace SimpleCalculator
                 result.Clear();
             }
             operation_pressed = false;
-            Button numBtn = (Button)sender; //convert the sender object to a button 
+            Button numBtn = (Button)sender; //convert the sender object to a button
             result.Text = result.Text + numBtn.Text;
+        }
 
+        private void button17_Click(object sender, EventArgs e)
+        {
+            result.Clear();
+            equation.Text = "";
+            value = 0;
         }
 
         private void button18_Click(object sender, EventArgs e)
         {
             result.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
 
         private void operator_click(object sender, EventArgs e)
@@ -54,39 +47,37 @@ namespace SimpleCalculator
             operation = opBtn.Text;
             value = Double.Parse(result.Text);
             equation.Text = value + operation;
-
         }
-
 
         private void res_click(object sender, EventArgs e)
         {
             operation_pressed = false;
             equation.Text = "";
-           switch (operation)
+            switch (operation)
             {
                 case "+":
                     result.Text = (value + Double.Parse(result.Text)).ToString();
                     break;
+
                 case "-":
                     result.Text = (value - Double.Parse(result.Text)).ToString();
                     break;
+
                 case "/":
                     result.Text = (value / Double.Parse(result.Text)).ToString();
                     break;
+
                 case "*":
                     result.Text = (value * Double.Parse(result.Text)).ToString();
                     break;
+
                 default:
                     break;
             }
-            
         }
 
-        private void button17_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            result.Clear();
-            equation.Text = "";
-            value = 0;
         }
     }
-    }
+}
