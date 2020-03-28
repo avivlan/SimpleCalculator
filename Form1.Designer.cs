4278aaf99838b08b076ff981823ff5e565800832
Aviv Landesman <aviv13523@gmail.com>
2020-03-28 14:53:08 +0300

    Disabled resizing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -299,6 +299,8 @@
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(276, 297);
+            this.MinimumSize = new System.Drawing.Size(276, 297);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleCalculator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple calulator program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleCalculator")]
